7f95fb8494c106ed19a9a8132ff7f461703174ea
Cihat Solak <cihatsolak@hotmail.com>
2021-10-23 15:32:30 +0300

    WheelManagerTest class created

---
+++ WheelyWebApplication/Wheely.Service.Test/WheelManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Wheely.Service.Wheels;
+using Xunit;
+
+namespace Wheely.Service.Test
+{
+    public sealed class WheelManagerTest
+    {
+        #region Fields
+        public Mock<IWheelService> WheelServiceMock { get; set; }
+        #endregion
+
+        #region Constructor
+        public WheelManagerTest()
+        {
+            WheelServiceMock = new Mock<IWheelService>();
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
