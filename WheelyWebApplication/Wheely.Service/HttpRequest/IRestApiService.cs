1d5532e53c6ee0747d06891f3b4e375d0cf21618
Cihat Solak <cihatsolak@hotmail.com>
2021-11-16 22:08:35 +0300

    Rest api service review

--- WheelyWebApplication/Wheely.Service/HttpRequest/IRestApiService.cs
+++ WheelyWebApplication/Wheely.Service/HttpRequest/IRestApiService.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace Wheely.Service.HttpRequest
 {
     public partial interface IRestApiService
     {
-        T PostApiResponse<T>(string serviceUri, object parameters, NameValueCollection headersCollection = null, string authToken = null) where T : class, new();
-        T GetApiResponse<T>(string serviceUri, NameValueCollection headersCollection = null) where T : class, new();
+        /// <summary>
+        /// Http post request
+        /// </summary>
+        /// <typeparam name="T">class and new()</typeparam>
+        /// <param name="serviceUrl">service url</param>
+        /// <param name="parameters">parameters</param>
+        /// <param name="headersCollection">header collection</param>
+        /// <param name="authToken">authorization token</param>
+        /// <returns></returns>
+        T PostApiResponse<T>(string serviceUrl, object parameters, NameValueCollection headersCollection = null, string authToken = null) where T : class, new();
+
+        /// <summary>
+        /// Http post request - Form Url Encoded Content
+        /// </summary>
+        /// <typeparam name="T">class and new()</typeparam>
+        /// <param name="serviceUrl">service url</param>
+        /// <param name="keyValuePairs">key value pairs</param>
+        /// <param name="authToken">authorization token</param>
+        /// <returns></returns>
+        T PostApiResponse<T>(string serviceUrl, IList<KeyValuePair<string, string>> keyValuePairs, string authToken = null) where T : class, new();
+
+        /// <summary>
+        /// Http get request
+        /// </summary>
+        /// <typeparam name="T">class and new()</typeparam>
+        /// <param name="serviceUrl">service url</param>
+        /// <param name="headersCollection">header collection</param>
+        /// <returns></returns>
+        T GetApiResponse<T>(string serviceUrl, NameValueCollection headersCollection = null) where T : class, new();
     }
 }
--- WheelyWebApplication/Wheely.Service/HttpRequest/RestApiManager.cs
+++ WheelyWebApplication/Wheely.Service/HttpRequest/RestApiManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Net.Http.Headers;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -12,9 +13,9 @@ namespace Wheely.Service.HttpRequest
     public sealed class RestApiManager : IRestApiService
     {
         #region Methods
-        public T GetApiResponse<T>(string serviceUri, NameValueCollection headersCollection = null) where T : class, new()
+        public T GetApiResponse<T>(string serviceUrl, NameValueCollection headersCollection = null) where T : class, new()
         {
-            var httpResponseMessage = GetRequest(serviceUri, headersCollection);
+            var httpResponseMessage = GetRequest(serviceUrl, headersCollection);
             if (!httpResponseMessage.IsSuccessStatusCode)
                 return null;
 
@@ -22,9 +23,19 @@ namespace Wheely.Service.HttpRequest
             return result;
         }
 
-        public T PostApiResponse<T>(string serviceUri, object parameters, NameValueCollection headersCollection = null, string authToken = null) where T : class, new()
+        public T PostApiResponse<T>(string serviceUrl, object parameters, NameValueCollection headersCollection = null, string authToken = null) where T : class, new()
         {
-            var httpResponseMessage = PostRequest(serviceUri, parameters, headersCollection, authToken);
+            var httpResponseMessage = PostRequest(serviceUrl, parameters, headersCollection, authToken);
+            if (!httpResponseMessage.IsSuccessStatusCode)
+                return null;
+
+            var result = httpResponseMessage.Content.ReadAsStringAsync().Result.AsModel<T>();
+            return result;
+        }
+
+        public T PostApiResponse<T>(string serviceUrl, IList<KeyValuePair<string, string>> keyValuePairs, string authToken = null) where T : class, new()
+        {
+            var httpResponseMessage = PostRequest(serviceUrl, keyValuePairs, authToken);
             if (!httpResponseMessage.IsSuccessStatusCode)
                 return null;
 
@@ -34,37 +45,6 @@ namespace Wheely.Service.HttpRequest
         #endregion
 
         #region Utitilies
-        private HttpResponseMessage PostRequest(string serviceUrl, object parameters, NameValueCollection headersCollection = null, string authToken = null)
-        {
-            using HttpClient client = new()
-            {
-                Timeout = TimeSpan.FromMinutes(1)
-            };
-            var strParam = parameters.ToJsonString();
-            StringContent stringContent = new(strParam, Encoding.UTF8, MediaTypeNames.Application.Json);
-
-            client.BaseAddress = new Uri(serviceUrl);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.ExpectContinue = false;
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
-
-            if (!string.IsNullOrEmpty(authToken))
-            {
-                client.DefaultRequestHeaders.Add(HeaderNames.Authorization, authToken);
-            }
-
-            if (headersCollection is not null)
-            {
-                foreach (var key in headersCollection.AllKeys)
-                {
-                    client.DefaultRequestHeaders.TryAddWithoutValidation(key, headersCollection[key]);
-                }
-            }
-
-            var httpResponseMessage = client.PostAsync(serviceUrl, stringContent).Result;
-            return httpResponseMessage;
-        }
-
         private HttpResponseMessage GetRequest(string serviceUrl, NameValueCollection headersCollection = null)
         {
             using HttpClient client = new()
@@ -86,6 +66,60 @@ namespace Wheely.Service.HttpRequest
 
             var httpResponseMessage = client.GetAsync(serviceUrl).Result;
             return httpResponseMessage;
+        }
+
+        private HttpResponseMessage PostRequest(string serviceUrl, object parameters, NameValueCollection headersCollection = null, string authToken = null)
+        {
+            using HttpClient client = new()
+            {
+                Timeout = TimeSpan.FromMinutes(1),
+                BaseAddress = new Uri(serviceUrl)
+            };
+
+            var strParam = parameters.ToJsonString();
+            StringContent stringContent = new(strParam, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.ExpectContinue = false;
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+
+            if (!string.IsNullOrEmpty(authToken))
+            {
+                client.DefaultRequestHeaders.Add(HeaderNames.Authorization, authToken);
+            }
+
+            if (headersCollection is not null)
+            {
+                foreach (var key in headersCollection.AllKeys)
+                {
+                    client.DefaultRequestHeaders.TryAddWithoutValidation(key, headersCollection[key]);
+                }
+            }
+
+            var httpResponseMessage = client.PostAsync(serviceUrl, stringContent).Result;
+            return httpResponseMessage;
+        }
+
+        private HttpResponseMessage PostRequest(string serviceUrl, IList<KeyValuePair<string, string>> postData, string authToken = null)
+        {
+            using var client = new HttpClient()
+            {
+                Timeout = TimeSpan.FromMinutes(1),
+                BaseAddress = new Uri(serviceUrl)
+            };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.ExpectContinue = false;
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+
+            if (!string.IsNullOrWhiteSpace(authToken))
+                client.DefaultRequestHeaders.Add(HeaderNames.Authorization, authToken);
+
+            using var content = new FormUrlEncodedContent(postData);
+            var request = new HttpRequestMessage(HttpMethod.Post, serviceUrl) { Content = content };
+
+            var result = client.SendAsync(request).Result;
+            return result;
         }
         #endregion
     }
